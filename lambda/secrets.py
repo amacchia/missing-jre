031c1229e221e971635abff0c6911b8c6505117c
Anthony Macchia <antmacchia15@gmail.com>
2021-05-08 17:54:14 -0400

    Add params for secrets

---
+++ lambda/secrets.py
@@ -0,0 +1,8 @@
+import os
+
+twitter_api_key = os.environ.get('TWITTER_API_KEY')
+twitter_secret_key = os.environ.get('TWITTER_SECRET_KEY')
+twitter_access_token = os.environ.get('TWITTER_ACCESS_TOKEN')
+twitter_token_secret = os.environ.get('TWITTER_TOKEN_SECRET')
+
+spotify_authorization = os.environ.get('SPOTIFY_AUTHORIZATION')
