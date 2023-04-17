c942870368453cde8561e89056861cca1ace802a
Anthony Macchia <antmacchia15@gmail.com>
2023-04-17 13:38:24 -0400

    Remove additional logging

--- lambda/find_missing_episodes.py
+++ lambda/find_missing_episodes.py
@@ -1,23 +1,17 @@
 import json
 import traceback
-import logging
 
 from spotify_client import get_filtered_episodes_from_spotify
 from twitter_client import tweet_missing_episodes
 
-logger = logging.getLogger()
 
 def main(event, lambda_context):
     try:
         spotify_episode_numbers = get_filtered_episodes_from_spotify()
-        logger.info("Retrieved episodes from spotify")
         all_episodes_dict = get_all_episodes_dict()
-        logger.info("Read in master list")
         missing_episodes = find_missing_episodes(
             all_episodes_dict, spotify_episode_numbers)
-        logger.info("Found missing episodes")
         tweet_missing_episodes(missing_episodes)
-        logger.info("Tweeted missing episodes")
     except Exception as ex:
         traceback.print_exception(type(ex), ex, ex.__traceback__)
 
